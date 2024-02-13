3be43e03191158cbf0340f1b038602f5a2197d6a
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-02-13 13:28:47 +0530

    init components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
+import { NavBar } from "@/sections/navbar";
 import styles from "./page.module.css";
+import HeroSection from "@/sections/hero";
+import Spotlight from "@/sections/spotlight";
+import MoreSection from "@/sections/more";
+import Footer from "@/sections/footer";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <NavBar />
+      <HeroSection />
+      <Spotlight />
+      <MoreSection />
+      <Footer />
+    </main>
+  );
 }
