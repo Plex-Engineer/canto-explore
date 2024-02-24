53c6f41d7e515e75ca784273a2a0b6c4ee727402
Guled <gyousuf@gmail.com>
2024-02-23 20:06:55 -0800

    Initial content addition

--- config/data.ts
+++ config/data.ts
@@ -2,80 +2,380 @@ import { CardProps } from "@/components/cards/highlightCard";
 
 const Spotlights: CardProps[] = [
   {
-    title: "Aave",
+    title: "1155.TECH",
     description:
-      "Aave is an open source and non-custodial protocol to earn interest on deposits and borrow assets.",
-    image: "/hero.png",
-    category: "LENDING",
-    links: {
-      site: "https://aave.com/",
-      twitter: "https://twitter.com/AaveAave",
-      github: "",
-    },
-  },
-  {
-    title: "Uniswap",
-    description:
-      "Uniswap is a protocol for automated token exchange on Ethereum.",
-    image: "/hero.png",
-    category: "DEX",
-    links: {
-      site: "https://uniswap.org/",
-      twitter: "https://twitter.com/Uniswap",
-      github: "",
-    },
-  },
-  {
-    title: "Rarible",
-    description:
-      "Create, sell or collect digital items secured with blockchain.",
-    image: "/hero.png",
+      "1155.tech offers a marketplace for digital art with a unique feature - users can earn yield each time a key they own is involved in a transaction, whether it’s bought, sold, or burnt. The platform is developed by Atrium, a top team in the crypto industry, and is backed by Electric Capital. They are also currently producing the NounsDAO movie.",
+    image: "/images/1155tech.png",
     category: "NFT",
     links: {
-      site: "https://rarible.com/",
-      twitter: "https://twitter.com/rariblecom",
+      site: "https://1155.tech/?rfc=378bde47-1571",
+      twitter: "",
+      github: "",
+    },
+  },
+  {
+    title: "Vivacity Finance",
+    description:
+      "Vivacity Finance offers an onchain, institutional grade lending market for real world assets on Canto. Users can lend their NOTE to borrowers to earn additional yield.",
+    image: "/images/vivacity.png",
+    category: "DeFi",
+    links: {
+      site: "https://vivacity.finance/",
+      twitter: "",
+      github: "",
+    },
+  },
+  {
+    title: "Blank Rasa",
+    description:
+      "Blank Rasa is a platform for discovering and trading NFTs. It features collections such as Canto Longneck, Shnoise, and Dead Ends.",
+    image: "/images/blankrasa.png",
+    category: "NFT",
+    links: {
+      site: "",
+      twitter: "",
       github: "",
     },
   },
 ];
 
 const Projects: CardProps[] = [
+  // Block Explorers
+  
   {
-    title: "Aave",
+    title: "Tuber",
     description:
-      "Aave is an open source and non-custodial protocol to earn interest on deposits and borrow assets.",
-    image: "/hero.png",
-    category: "LENDING",
+      "Canto EVM block explorer based on Blockscout. Maintained by Neobase.",
+    image: "images/tuber.svg",
+    category: "Block Explorer",
     links: {
-      site: "https://aave.com/",
-      twitter: "https://twitter.com/AaveAave",
+      site: "https://tuber.build/",
+      twitter: "",
       github: "",
     },
   },
   {
-    title: "Uniswap",
+    title: "Mintscan",
     description:
-      "Uniswap is a protocol for automated token exchange on Ethereum.",
-    image: "/hero.png",
-    category: "DEX",
+      "A multi-chain native Canto chain block explorer.",
+    image: "/images/hero.png",
+    category: "Block Explorer",
     links: {
-      site: "https://uniswap.org/",
-      twitter: "https://twitter.com/Uniswap",
+      site: "https://www.mintscan.io/canto",
+      twitter: "",
       github: "",
     },
   },
   {
-    title: "Rarible",
+    title: "OKLink",
     description:
-      "Create, sell or collect digital items secured with blockchain.",
-    image: "/hero.png",
+      "A multi-chain EVM blockchain explorer.",
+    image: "/images/hero.png",
+    category: "Block Explorer",
+    links: {
+      site: "https://www.oklink.com/canto",
+      twitter: "",
+      github: "",
+    },
+  },
+  {
+    title: "DexGuru",
+    description:
+      "EVM Canto chain block explorer.",
+    image: "/images/hero.png",
+    category: "Block Explorer",
+    links: {
+      site: "https://canto.dex.guru/",
+      twitter: "",
+      github: "",
+    },
+  },
+  {
+    title: "Parsec",
+    description:
+      "Customizable onchain analytics terminal and block explorer.",
+    image: "/images/hero.png",
+    category: "Block Explorer",
+    links: {
+      site: "https://parsec.fi/",
+      twitter: "",
+      github: "",
+    },
+  },
+
+    // Bridges
+
+  {
+    title: "Synapse",
+    description: "Cross chain bridge.",
+    image: "/images/hero.png",
+    category: "Bridge",
+    links: {
+      site: "https://synapseprotocol.com/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Celer",
+    description: "Cross chain bridge.",
+    image: "/images/hero.png",
+    category: "Bridge",
+    links: {
+      site: "https://celer.network/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "LayerZero",
+    description: "An omnichain interoperability protocol.",
+    image: "/images/hero.png",
+    category: "Bridge",
+    links: {
+      site: "https://layerzero.network/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Gravity Bridge",
+    description: "Gravity Bridge is a decentralized Cosmos blockchain whose primary purpose is to secure the operation of bridges between blockchains.",
+    image: "/images/hero.png",
+    category: "Bridge",
+    links: {
+      site: "https://www.gravitybridge.net/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+    // DeFi
+
+  {
+    title: "Beefy",
+    description: "Multichain Yield Optimizer. Autocompound your tokens and positions.",
+    image: "/images/hero.png",
+    category: "DeFi",
+    links: {
+      site: "https://beefy.finance/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Cadence",
+    description: "Next generation decentralized perpetuals. Built on Canto by ex-ConsenSys, Kraken & Amazon devs.",
+    image: "/images/hero.png",
+    category: "DeFi",
+    links: {
+      site: "https://www.cadenceprotocol.io/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Slingshot",
+    description: "Premier DEX on Canto.",
+    image: "/images/hero.png",
+    category: "DeFi",
+    links: {
+      site: "https://slingshot.finance/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Vivacity Finance",
+    description: "Lending market for onchain, institutional-grade real world assets.",
+    image: "/images/hero.png",
+    category: "DeFi",
+    links: {
+      site: "https://vivacity.finance/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+    // RWA
+
+  {
+    title: "Fortunafi",
+    description: "All-in-one liquidity solutions for stablecoin issuers, protocol treasuries, and traditional entities.",
+    image: "/images/hero.png",
+    category: "RWA",
+    links: {
+      site: "https://www.fortunafi.com/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Hashnote",
+    description: "Regulated asset management with customizable digital and real world asset exposure.",
+    image: "/images/hero.png",
+    category: "RWA",
+    links: {
+      site: "https://www.hashnote.com/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Anzen",
+    description: "RWA Issuer with a focus on high-quality private credit deals with consistent returns.",
+    image: "/images/hero.png",
+    category: "RWA",
+    links: {
+      site: "https://anzen.finance/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "HiYield",
+    description: "Real-world assets, curated on-chain",
+    image: "/images/hero.png",
+    category: "RWA",
+    links: {
+      site: "https://www.hiyield.xyz/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+    // NFTs
+
+  {
+    title: "1155.Tech",
+    description: "Collect keys, Unlock art. Now live on Canto.",
+    image: "/images/hero.png",
     category: "NFT",
     links: {
-      site: "https://rarible.com/",
-      twitter: "https://twitter.com/rariblecom",
-      github: "",
-    },
+      site: "https://1155.tech/",
+      twitter: "",
+      github: ""
+    }
   },
+  {
+    title: "Blank Rasa",
+    description: "Blank Rasa is the NFT marketplace that fiercely defends artist royalties. Trade collectibles and mint with support for ERC-1155, html, mp3, and mp4.",
+    image: "/images/hero.png",
+    category: "NFT",
+    links: {
+      site: "https://www.blankrasa.com/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+    // Identity
+
+  {
+    title: "Canto Identity Protocol",
+    description: "An on-chain identity protocol that aggregates identity from Subprotocols.",
+    image: "/images/hero.png",
+    category: "NFT",
+    links: {
+      site: "https://www.cantoidentity.build/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Canto Namespace Protocol",
+    description: "Canto Namespace Protocol is a subprotocol for the Canto Identity Protocol that enables users to mint names with tiles that are contained in trays.",
+    image: "/images/hero.png",
+    category: "NFT",
+    links: {
+      site: "https://www.cantoname.build/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+    // Infra & Tools
+
+  {
+    title: "Redstone",
+    description: "Redstone provides modular oracles.",
+    image: "/images/hero.png",
+    category: "Infra & Tools",
+    links: {
+      site: "https://redstone.finance/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Ansybl",
+    description: "Access to fast, reliable, and secure API endpoints & nodes for the Canto ecosystem.",
+    image: "/images/hero.png",
+    category: "Infra & Tools",
+    links: {
+      site: "https://www.ansybl.io/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Pyth",
+    description: "Pyth delivers real-time market data from institutions to smart contracts.",
+    image: "/images/hero.png",
+    category: "Infra & Tools",
+    links: {
+      site: "https://pyth.network/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Transpose",
+    description: "Explore and integrate real-time blockchain data into any application at scale.",
+    image: "/images/hero.png",
+    category: "Infra & Tools",
+    links: {
+      site: "https://www.transpose.io/",
+      twitter: "",
+      github: ""
+    }
+  },
+
+  // Community
+
+  {
+    title: "Chandra Station",
+    description: "Infrastructure provider and Canto validator.",
+    image: "/images/hero.png",
+    category: "Community",
+    links: {
+      site: "https://www.chandrastation.com/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Four Moons",
+    description: "Canto validator and active community members.",
+    image: "/images/hero.png",
+    category: "Community",
+    links: {
+      site: "https://www.fourmoons.xyz/",
+      twitter: "",
+      github: ""
+    }
+  },
+  {
+    title: "Polkachu",
+    description: "Experienced team providing validation services for Canto.",
+    image: "/images/hero.png",
+    category: "Community",
+    links: {
+      site: "https://www.polkachu.com/",
+      twitter: "",
+      github: ""
+    }
+  }
 ];
 
 const More: CardProps[] = [
@@ -86,35 +386,11 @@ const More: CardProps[] = [
     image: "/hero.png",
     category: "LENDING",
     links: {
-      site: "https://aave.com/",
-      twitter: "https://twitter.com/AaveAave",
+      site: "https://tuber.build/",
+      twitter: "",
       github: "",
     },
-  },
-  {
-    title: "Uniswap",
-    description:
-      "Uniswap is a protocol for automated token exchange on Ethereum.",
-    image: "/hero.png",
-    category: "DEX",
-    links: {
-      site: "https://uniswap.org/",
-      twitter: "https://twitter.com/Uniswap",
-      github: "",
-    },
-  },
-  {
-    title: "Rarible",
-    description:
-      "Create, sell or collect digital items secured with blockchain.",
-    image: "/hero.png",
-    category: "NFT",
-    links: {
-      site: "https://rarible.com/",
-      twitter: "https://twitter.com/rariblecom",
-      github: "",
-    },
-  },
+  }
 ];
 
 export { Spotlights, Projects, More };
