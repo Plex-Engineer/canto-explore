ef5142da2bb759cc1c53755c28e58b0e900f3a20
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-02-27 04:33:10 +0530

    Update styles in cards and footer modules

--- sections/footer/footer.tsx
+++ sections/footer/footer.tsx
@@ -4,26 +4,28 @@ import styles from "./footer.module.scss";
 const Footer = () => {
   return (
     <div className={styles.container}>
-      <FooterCard
-        text="Visit Blog"
-        image="/blog.svg"
-        link="https://docs.canto.io/"
-      />
-      <FooterCard
-        text="Read Documentation"
-        image="/doc.svg"
-        link="https://docs.canto.io/"
-      />
-      <FooterCard
-        text="Join our Discord"
-        image="/discord.svg"
-        link="https://discord.com/invite/63GmEXZsVf"
-      />
-      <FooterCard
-        text="Follow Canto Public"
-        image="/twitter.svg"
-        link="https://twitter.com/CantoPublic"
-      />
+      <div className={styles["inner_container"]}>
+        <FooterCard
+          text="Visit Blog"
+          image="/blog.svg"
+          link="https://docs.canto.io/"
+        />
+        <FooterCard
+          text="Read Documentation"
+          image="/doc.svg"
+          link="https://docs.canto.io/"
+        />
+        <FooterCard
+          text="Join our Discord"
+          image="/discord.svg"
+          link="https://discord.com/invite/63GmEXZsVf"
+        />
+        <FooterCard
+          text="Follow Canto Public"
+          image="/twitter.svg"
+          link="https://twitter.com/CantoPublic"
+        />
+      </div>
     </div>
   );
 };
