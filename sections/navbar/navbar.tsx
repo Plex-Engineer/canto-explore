1536a02ec20b6e6ed34c897e549db5fb7ff4f82c
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-02-26 22:23:59 +0530

    Add link to logo in navbar

--- sections/navbar/navbar.tsx
+++ sections/navbar/navbar.tsx
@@ -1,16 +1,19 @@
 import Image from "next/image";
 import styles from "./navbar.module.scss";
+import Link from "next/link";
 
 export const NavBar = () => {
   return (
     <div className={styles.container}>
-      <Image
-        className={styles.logo}
-        src="/logo.svg"
-        alt="logo"
-        width={100}
-        height={100}
-      />
+      <Link href={"https://canto.io"}>
+        <Image
+          className={styles.logo}
+          src="/logo.svg"
+          alt="logo"
+          width={100}
+          height={100}
+        />
+      </Link>
 
       <div className={styles.stats}></div>
     </div>
