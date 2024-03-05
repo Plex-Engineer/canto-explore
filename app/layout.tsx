a08885cd208d9389c6c288fad8c7f27e46ee6d97
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-03-05 14:06:06 +0530

    Add keywords and update OpenGraph image in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,12 +16,20 @@ export const metadata: Metadata = {
   title: "Explore Canto’s Ecosystem",
   description:
     "Explore web+! Check out projects and integrations, discover upcoming community events, and stay updated with new developments in Canto’s ecosystem.",
+  keywords: ["Canto", "web+", "blockchain", "ecosystem"],
   openGraph: {
     title: "Explore Canto’s Ecosystem",
     description:
       "Explore web+! Check out projects and integrations, discover upcoming community events, and stay updated with new developments in Canto’s ecosystem.",
     type: "website",
-    images: ["/social.jpg"],
+    images: [
+      {
+        url: "https://explore.canto.io/social.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Canto’s Ecosystem",
+      },
+    ],
   },
 };
 
