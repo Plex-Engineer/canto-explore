64c0296fcf6470b982a8176fd43ab71ccf8ec81d
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-03-21 01:45:12 +0530

    Add CardOutlined component and update style.module.scss

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Card from "@/components/card";
 import Text from "@/components/text";
 import Gap from "@/components/gap";
+import { CardOutlined } from "@/components/cardOutline";
 
 export default function Home() {
   return (
@@ -40,51 +41,12 @@ export default function Home() {
         </Text>
         <Gap height={32} />
 
-        <Card
-          style={{
-            flexDirection: "row",
-            gap: "24px",
-          }}
-        >
-          <div
-            style={{
-              height: "350px",
-              flex: 1,
-            }}
-          >
-            <Image
-              src={"/cyclone.png"}
-              alt="Canto Bridge"
-              width={350}
-              height={350}
-              style={{
-                width: "100%",
-                opacity: 0.7,
-              }}
-            />
-          </div>
-          <div
-            className="content"
-            style={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              gap: "16px",
-            }}
-          >
-            <Text family="primary" size="medium" className={styles.title}>
-              CYCLONE Stack: A Parallel Execution EVM
-            </Text>
-            <Text color="secondary" size="small" weight="regular">
-              Experience the speed and efficiency unlocked by Canto’s Parallel
+        <CardOutlined
+          title="CYCLONE Stack: A Parallel Execution EVM"
+          content="Experience the speed and efficiency unlocked by Canto’s Parallel
               Execution Cyclone EVM. It combines speed with scalability to give
-              developers the best possible platform for building applications.
-            </Text>
-            <div
-              style={{
-                flex: 1,
-              }}
-            ></div>
+              developers the best possible platform for building applications."
+          extra={
             <div
               style={{
                 backgroundColor: "#131313",
@@ -96,8 +58,8 @@ export default function Home() {
                 <span>5,000 TPS</span> / Sub-Second Block Time
               </Text>
             </div>
-          </div>
-        </Card>
+          }
+        />
       </section>
       <Gap height={48} />
       <section className={styles.highlight}>
@@ -111,91 +73,19 @@ export default function Home() {
             gap: "24px",
           }}
         >
-          <Card
-            style={{
-              gap: "24px",
-              flexDirection: "column",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                height: "350px",
-                flex: 1,
-              }}
-            >
-              <Image
-                src={"/cyclone.png"}
-                alt="Canto Bridge"
-                width={350}
-                height={350}
-                style={{
-                  width: "100%",
-                  opacity: 0.7,
-                }}
-              />
-            </div>
-            <div
-              className="content"
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-              }}
-            >
-              <Text family="primary" size="medium" className={styles.title}>
-                NEOFINANCE
-              </Text>
-              <Text color="secondary" size="small" weight="regular">
-                A new era of financial efficiencies, unlocked at scale, by
-                deploying off chain assets on protocol rails.
-              </Text>
-            </div>
-          </Card>
-          <Card
-            style={{
-              gap: "24px",
-              flexDirection: "column",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                height: "350px",
-                flex: 1,
-              }}
-            >
-              <Image
-                src={"/cyclone.png"}
-                alt="Canto Bridge"
-                width={350}
-                height={350}
-                style={{
-                  width: "100%",
-                  opacity: 0.7,
-                }}
-              />
-            </div>
-            <div
-              className="content"
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-              }}
-            >
-              <Text family="primary" size="medium" className={styles.title}>
-                Contract secured revenue
-              </Text>
-              <Text color="secondary" size="small" weight="regular">
-                A distinct mechanism that enables novel methods of funding. 20%
-                of gas spent on an application is awarded to developers in a
-                composable manner.
-              </Text>
-            </div>
-          </Card>
+          <CardOutlined
+            title="NEOFINANCE"
+            content=" A new era of financial efficiencies, unlocked at scale, by
+            deploying off chain assets on protocol rails."
+            direction="column"
+          />
+          <CardOutlined
+            title="Contract secured revenue"
+            content="A distinct mechanism that enables novel methods of funding. 20%
+            of gas spent on an application is awarded to developers in a
+            composable manner."
+            direction="column"
+          />
         </div>
       </section>
       <Gap height={48} />
@@ -210,129 +100,24 @@ export default function Home() {
             gap: "24px",
           }}
         >
-          <Card
-            style={{
-              gap: "24px",
-              flexDirection: "column",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                height: "220px",
-                flex: 1,
-              }}
-            >
-              <Image
-                src={"/cyclone.png"}
-                alt="Canto Bridge"
-                width={350}
-                height={220}
-                style={{
-                  width: "100%",
-                  opacity: 0.7,
-                }}
-              />
-            </div>
-            <div
-              className="content"
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-              }}
-            >
-              <Text family="primary" size="medium" className={styles.title}>
-                CANTo DEX
-              </Text>
-              <Text color="secondary" size="small" weight="regular">
-                A zero-fee DEX, fueled by L1 incentives
-              </Text>
-            </div>
-          </Card>
-          <Card
-            style={{
-              gap: "24px",
-              flexDirection: "column",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                height: "220px",
-                flex: 1,
-              }}
-            >
-              <Image
-                src={"/cyclone.png"}
-                alt="Canto Bridge"
-                width={350}
-                height={220}
-                style={{
-                  width: "100%",
-                  opacity: 0.7,
-                }}
-              />
-            </div>
-            <div
-              className="content"
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-              }}
-            >
-              <Text family="primary" size="medium" className={styles.title}>
-                CANTo Lending market
-              </Text>
-              <Text color="secondary" size="small" weight="regular">
-                A lending market focused on NOTE & RWAs emphasizing security.
-              </Text>
-            </div>
-          </Card>
-          <Card
-            style={{
-              gap: "24px",
-              flexDirection: "column",
-              width: "100%",
-            }}
-          >
-            <div
-              style={{
-                height: "220px",
-                flex: 1,
-              }}
-            >
-              <Image
-                src={"/cyclone.png"}
-                alt="Canto Bridge"
-                width={350}
-                height={220}
-                style={{
-                  width: "100%",
-                  opacity: 0.7,
-                }}
-              />
-            </div>
-            <div
-              className="content"
-              style={{
-                flex: 1,
-                display: "flex",
-                flexDirection: "column",
-                gap: "16px",
-              }}
-            >
-              <Text family="primary" size="medium" className={styles.title}>
-                NOte
-              </Text>
-              <Text color="secondary" size="small" weight="regular">
-                Canto’s native unit of account backed by RWAs.
-              </Text>
-            </div>
-          </Card>
+          <CardOutlined
+            title="Canto Dex"
+            content="A zero-fee DEX, fueled by L1 incentives"
+            direction="column"
+            imgHeight="220px"
+          />
+          <CardOutlined
+            title="Canto Lending market"
+            content="A lending market focused on NOTE & RWAs emphasizing security."
+            direction="column"
+            imgHeight="220px"
+          />
+          <CardOutlined
+            title="Note"
+            content="Canto’s native unit of account backed by RWAs."
+            direction="column"
+            imgHeight="220px"
+          />
         </div>
       </section>
     </main>
---
+++ components/cardOutline.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import React from "react";
+import Card from "./card";
+import Text from "./text";
+import styles from "./style.module.scss";
+
+interface Props {
+  title: string;
+  content: string;
+  direction?: "row" | "column";
+  extra?: React.ReactNode;
+  imgHeight?: string;
+}
+export const CardOutlined = ({
+  title,
+  content,
+  direction,
+  extra,
+  imgHeight,
+}: Props) => {
+  return (
+    <Card
+      className={styles.cardOutlined}
+      style={{
+        flexDirection: direction,
+        gap: "24px",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          height: imgHeight ? imgHeight : "350px",
+          flex: 1,
+        }}
+      >
+        <Image
+          src={"/cyclone.png"}
+          alt="Canto Bridge"
+          width={Number((imgHeight ? imgHeight : "350px").replace("px", ""))}
+          height={Number((imgHeight ? imgHeight : "350px").replace("px", ""))}
+          style={{
+            width: "100%",
+            opacity: 0.7,
+          }}
+        />
+      </div>
+      <div
+        className="content"
+        style={{
+          flex: 1,
+          display: "flex",
+          flexDirection: "column",
+          gap: "16px",
+        }}
+      >
+        <Text family="primary" size="medium" className={styles.title}>
+          {title}
+        </Text>
+        <Text color="secondary" size="small" weight="regular">
+          {content}
+        </Text>
+        <div
+          style={{
+            flex: 1,
+          }}
+        ></div>
+        {extra}
+      </div>
+    </Card>
+  );
+};
