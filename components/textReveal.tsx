6e86306dd38d11bed3a13ba20480600bf164cc7e
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-04-03 00:09:33 +0530

    Fix import statement in textReveal component

--- components/textReveal.tsx
+++ components/textReveal.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
 import styles from "./style.module.scss";
 
 interface Props {
@@ -17,13 +17,12 @@ export default function TextReveal({ text }: Props) {
 		if (start) {
 			let i = 0;
 			let j = text.length - 1;
-
 			let interval = setInterval(() => {
 				setAniText(text.slice(0, i + 1));
 				setAniTextReverse((prev) => prev.slice(1, aniTextReverse.length));
 				i++;
 				j--;
-				if (i === 10) {
+				if (i === text.length) {
 					clearInterval(interval);
 				}
 			}, speed);
