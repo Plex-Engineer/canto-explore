14f3a8e78d7bdd5b8f474f057f1d05d7f487e757
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-04-02 23:48:36 +0530

    Add blinking cursor animation to text reveal component

--- components/textReveal.tsx
+++ components/textReveal.tsx
@@ -1,43 +1,63 @@
-import { motion } from "framer-motion";
+import { use, useEffect, useState } from "react";
 import styles from "./style.module.scss";
-import ReactTypingEffect from "react-typing-effect";
 
 interface Props {
 	text: string;
+	cursor: string;
+	speed: number;
 }
 
 export default function TextReveal({ text }: Props) {
+	const [start, setStart] = useState(false);
+
+	const [aniText, setAniText] = useState("");
+	const [aniTextReverse, setAniTextReverse] = useState(text);
+
+	// randomize the speed of timeout to make it more natural
+	const speed = Math.floor(Math.random() * 100) + 50;
+	useEffect(() => {
+		if (start) {
+			let i = 0;
+			let j = text.length - 1;
+
+			let interval = setInterval(() => {
+				setAniText(text.slice(0, i + 1));
+				setAniTextReverse((prev) => prev.slice(1, aniTextReverse.length));
+				i++;
+				j--;
+				if (i === 10) {
+					clearInterval(interval);
+				}
+			}, speed);
+		}
+	}, [start]);
+
+	//a version of textReveal where after each word the speed of the text reveal changes
+
+	useEffect(() => {
+		setTimeout(() => {
+			setStart(true);
+		}, 1000);
+	}, []);
 	return (
-		<div
-			style={{
-				position: "relative",
-				display: "inline-block",
-			}}
-		>
-			<p
-				className={styles.textReveal}
+		<span className={styles.textReveal}>
+			{/* keep adding text after delay */}
+			{aniText}
+			<span
 				style={{
-					opacity: 0.4,
-					display: "flex",
-				}}
-			>
-				{text}
-			</p>
-			<div
-				style={{
-					display: "inline-block",
 					position: "absolute",
-					top: 0,
+				}}
+				className={styles.cursor}
+			>
+				_
+			</span>
+			<span
+				style={{
+					opacity: 0.5,
 				}}
 			>
-				<ReactTypingEffect
-					speed={50}
-					cursor="_"
-					typingDelay={1000}
-					className={`${styles.textReveal} `}
-					text={text}
-				/>
-			</div>
-		</div>
+				{aniTextReverse}
+			</span>
+		</span>
 	);
 }
