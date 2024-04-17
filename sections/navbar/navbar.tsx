956eba8ab12246d6c93f9659957d528bde83da26
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-04-17 20:16:44 +0530

    Update navbar styling and remove unused code

--- sections/navbar/navbar.tsx
+++ sections/navbar/navbar.tsx
@@ -7,58 +7,58 @@ import { usePostHog } from "posthog-js/react";
 import { usePathname } from "next/navigation";
 
 export const NavBar = () => {
-  const posthog = usePostHog();
+	const posthog = usePostHog();
 
-  const pathName = usePathname();
+	const pathName = usePathname();
 
-  return (
-    <div className={styles.container}>
-      <Link
-        className={styles.logo}
-        href={"https://canto.io"}
-        onClick={() =>
-          posthog.capture("External Link Clicked", { Website: "Canto Logo" })
-        }
-      >
-        <Image
-          className={styles.logo}
-          src="/logo-3d.png"
-          alt="logo"
-          width={100}
-          height={100}
-        />
-      </Link>
+	return (
+		<div className={styles.container}>
+			<Link
+				className={styles.logo}
+				href={"https://canto.io"}
+				onClick={() =>
+					posthog.capture("External Link Clicked", { Website: "Canto Logo" })
+				}
+			>
+				<Image
+					className={styles.logo}
+					src="/logo-3d.png"
+					alt="logo"
+					width={100}
+					height={100}
+				/>
+			</Link>
 
-      <ul className={styles.links}>
-        <Link href={"/"} className={pathName === "/" ? styles.active : ""}>
-          Home
-        </Link>
-        <Link
-          href={"/eco"}
-          className={pathName === "/eco" ? styles.active : ""}
-        >
-          Ecosystem
-        </Link>
-        <Link
-          href={"/dev"}
-          className={pathName === "/dev" ? styles.active : ""}
-        >
-          Developers
-        </Link>
-      </ul>
+			<ul className={styles.links}>
+				<Link href={"/"} className={pathName === "/" ? styles.active : ""}>
+					Home
+				</Link>
+				<Link
+					href={"/eco"}
+					className={pathName === "/eco" ? styles.active : ""}
+				>
+					Ecosystem
+				</Link>
+				<Link
+					href={"/dev"}
+					className={pathName === "/dev" ? styles.active : ""}
+				>
+					Developers
+				</Link>
+			</ul>
 
-      <ul className={styles.buttons}>
-        <Link
-          href={"/"}
-          onClick={() =>
-            posthog.capture("Navlink", {
-              Website: "Launch Homepage",
-            })
-          }
-        >
-          Launch App
-        </Link>
-      </ul>
-    </div>
-  );
+			<ul className={styles.buttons}>
+				<Link
+					href={"http://app.canto.io/bridge"}
+					onClick={() =>
+						posthog.capture("Navlink", {
+							Website: "Launch Homepage",
+						})
+					}
+				>
+					Launch App
+				</Link>
+			</ul>
+		</div>
+	);
 };
