f13a12e0844655fd0231432389ccfb7006326c7e
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-05-01 01:53:56 +0530

    Refactor image paths in featured and highlights components

--- app/eco/components/cards/footerCard.tsx
+++ app/eco/components/cards/footerCard.tsx
@@ -3,34 +3,34 @@
 import Image from "next/image";
 import styles from "./cards.module.scss";
 import Link from "next/link";
-import { usePostHog } from 'posthog-js/react';
+import { usePostHog } from "posthog-js/react";
 
 interface Props {
-  text: string;
-  title: string,
-  image: string;
-  link: string;
+	text: string;
+	title: string;
+	image: string;
+	link: string;
 }
 
 const FooterCard = (props: Props) => {
-  const posthog = usePostHog();
-  return (
-    // new tab
-    <Link
-      className={styles.footerCard}
-      href={props.link}
-      target="_blank"
-      onClick={() =>
-        posthog.capture("External Link Clicked", { Website: props.title })
-      }
-    >
-      <div>
-        <Image src={props.image} alt={"link"} height={24} width={24} />
-        {props.text}
-      </div>
-      <Image src={"/link.svg"} alt={"link"} height={32} width={32} />
-    </Link>
-  );
+	const posthog = usePostHog();
+	return (
+		// new tab
+		<Link
+			className={styles.footerCard}
+			href={props.link}
+			target="_blank"
+			onClick={() =>
+				posthog.capture("External Link Clicked", { Website: props.title })
+			}
+		>
+			<div>
+				<Image src={props.image} alt={"link"} height={24} width={24} />
+				{props.text}
+			</div>
+			{/* <Image src={"/link.svg"} alt={"link"} height={32} width={32} /> */}
+		</Link>
+	);
 };
 
 export default FooterCard;
