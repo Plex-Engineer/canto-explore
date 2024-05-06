44ebf22e9d435fc88bea924c8e219db1f266d0e1
bharadwaj palakurthy <bharadwajpalakurthy@gmail.com>
2024-05-07 00:39:43 +0530

    chore: Refactor navbar styles and add mobile menu functionality

--- sections/navbar/navbar.tsx
+++ sections/navbar/navbar.tsx
@@ -5,20 +5,47 @@ import styles from "./navbar.module.scss";
 import Link from "next/link";
 import { usePostHog } from "posthog-js/react";
 import { usePathname } from "next/navigation";
+import { use, useEffect, useState } from "react";
+import { useScroll } from "framer-motion";
+import { useScrollLock } from "@/hooks/useScrollLock";
 
 export const NavBar = () => {
 	const posthog = usePostHog();
 
 	const pathName = usePathname();
 
+	const [isMenuOpen, setMenuOpen] = useState(false);
+
+	const { lockScroll, unlockScroll } = useScrollLock();
+
+	useEffect(() => {
+		if (isMenuOpen) {
+			lockScroll(0);
+		} else {
+			unlockScroll();
+		}
+	}, [isMenuOpen, lockScroll, unlockScroll]);
+
 	return (
 		<div className={styles.container}>
+			<button
+				className={styles.menuButton}
+				onClick={() => {
+					setMenuOpen(!isMenuOpen);
+				}}
+			>
+				<Image src="/menu.svg" alt="menu" width={24} height={24} />
+			</button>
 			<Link
 				className={styles.logo}
 				href={"https://canto.io"}
-				onClick={() =>
-					posthog.capture("External Link Clicked", { Website: "Canto Logo" })
-				}
+				onClick={() => {
+					setMenuOpen(false);
+
+					return posthog.capture("External Link Clicked", {
+						Website: "Canto Logo",
+					});
+				}}
 			>
 				<Image
 					className={styles.logo}
@@ -29,13 +56,28 @@ export const NavBar = () => {
 				/>
 			</Link>
 
-			<ul className={styles.links}>
-				<Link href={"/"} className={pathName === "/" ? styles.active : ""}>
+			<ul className={`${styles.links} ${isMenuOpen ? styles.menuActive : ""}`}>
+				<Link
+					href={"/"}
+					className={pathName === "/" ? styles.active : ""}
+					onClick={() => {
+						setMenuOpen(false);
+						return posthog.capture("Navlink", {
+							Website: "Home",
+						});
+					}}
+				>
 					Home
 				</Link>
 				<Link
 					href={"/eco"}
 					className={pathName === "/eco" ? styles.active : ""}
+					onClick={() => {
+						setMenuOpen(false);
+						return posthog.capture("Navlink", {
+							Website: "Ecosystem",
+						});
+					}}
 				>
 					Ecosystem
 				</Link>
